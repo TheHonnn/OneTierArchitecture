8d36f80f69afb4db8a5c4a720777a762002f417d
TheHonnn <hbui73444@gmail.com>
2026-05-08 11:48:56 +0700

    Initial commit - ignored system files

---
+++ TodoApp/TodoRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    public class TodoRepository
+    {
+        private readonly List<Todo> _todos = new();
+        private string _filepath = "rodos.txt";
+        private int _nextId = 1;
+
+        public TodoRepository()
+        {
+            LoadFormFile();
+        }
+
+        private void LoadFormFile()
+        {
+            if (!File.Exists(_filepath)) return;
+            foreach (var line in File.ReadAllLines(_filepath))
+            {
+                var todo = Todo.FromFileString(line);
+                _todos.Add(todo);
+                if (todo.Id >= _nextId)
+                {
+                    _nextId = todo.Id++;
+                }
+            }
+        }
+        private void SaveChanges()
+        {
+            File.WriteAllLines(
+                _filepath,
+                _todos.Select(x =>x.ToFileString())
+                );
+        }
+        public Todo CreateTodo(string title)
+        {
+            var todo = new Todo()
+            {
+                Title = title,
+                Id = _nextId++,
+                IsSuccess = false,
+            };
+            _todos.Add(todo);
+            SaveChanges();
+            return todo;
+
+        }
+        public bool UpdateTodo(int id, string title) {
+            var item = _todos.FirstOrDefault(X => X.Id == id);
+                if (item != null) {
+                    item.Title = title;
+                    SaveChanges();
+                    return true;
+                }
+                return false;
+        }
+        public bool DeleteTodo (int id)
+        {
+            var item = _todos.FirstOrDefault(X => X.Id == id);
+            if (item != null)
+            {
+                _todos.Remove(item);
+                SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        public List<Todo> GetTodos() => _todos;
+        public bool ToggleTodo(int id) {
+            var item = _todos.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                item.IsSuccess = !item.IsSuccess;
+                SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+    
+}
---
+++ TodoApp/TodoUI.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    public class TodoUI
+    {
+        private readonly TodoService _service = new();
+        public void ShowTools() { 
+            var todos = _service.GetAll();
+            Console.WriteLine("DANH SACH CONG VIEC");
+            foreach (var todo in todos){
+                Console.WriteLine(todo.ToString());
+
+
+            }
+            if (todos.Count <= 0)
+            {
+                Console.WriteLine("Chua co cong viec");
+            }
+        }
+        public void ShowMenu()
+        {
+            Console.WriteLine("\n Chuc Nang");
+            Console.WriteLine(" 1. Them chuc nang");
+            Console.WriteLine(" 2. Danh dau da hoan thanh");
+            Console.WriteLine(" 3. Sua cong viec");
+            Console.WriteLine(" 4. Xoa cong viec");
+            Console.WriteLine(" 0. Thoat");
+        }
+        private void AddTodo() {
+            Console.WriteLine("Nhap dung cong viec: ");
+            var input = Console.ReadLine();
+            _service.AddTodo(input);
+        }
+        private void RemoveTodo()
+        {
+            Console.Write("Nhập Id công việc muốn xóa: ");
+            int id = int.Parse(Console.ReadLine());
+            _service.DeleteTodo(id);
+        }
+        private void DeleteTodo()
+        {
+            Console.Write("Nhập Id công việc muốn xóa: ");
+            int id = int.Parse(Console.ReadLine());
+            _service.DeleteTodo(id);
+        }
+        private void ToggleTodo()
+        {
+            Console.Write("Nhập Id công việc muốn : ");
+            int id = int.Parse(Console.ReadLine());
+            _service.ToggleTodo(id);
+        }
+        private void UpdateTodo()
+            
+        {
+            
+            Console.Write("Nhập Id công việc muốn danh dau: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Nhap noi dung moi: ");
+            string content = Console.ReadLine();
+            _service.UpdateTodo(id, content);
+            
+        }
+        public void Run() {
+            while (true)
+            {
+                Console.Clear();
+                ShowTools();
+                ShowMenu();
+                Console.Write("chon: ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        AddTodo();
+                        break;
+                    case "2":
+                        ToggleTodo();
+                        break;
+                    case "3":
+                        UpdateTodo();
+                        break;
+                    case "4":
+                        DeleteTodo();
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Lua chon khong hop le");
+                        break;
+
+                }
+                Console.Write("Nhan enter de tiep tuc");
+                Console.Write(" Nhan 0 de thoat.");
+            }
+        }
+    } 
+}
